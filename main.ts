ccc2523116efb20c3281705b18c3eb0d3a03537d
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-01-10 01:43:00 +0000

    added prototype

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Jan 9 2025
+ * This program rc car 
 */
 
-basic.showString('Hello, World!')
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    receivedString = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (receivedString == "up") {
+        robotbit.StpCarMove(2, 48)
+    }
+    if (receivedString == "down") {
+        robotbit.StpCarMove(-2, 48)
+    }
+    if (receivedString == "right") {
+        // Turning 90 degrees
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+    }
+    if (receivedString == "left") {
+         // Turning 90 degrees
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+    }
+    if (receivedString == "stop") {
+        robotbit.MotorRun(robotbit.Motors.M1A, 0)
+    }
+})
+let receivedString = ""
+bluetooth.startUartService()
+basic.showIcon(IconNames.Square)
+
